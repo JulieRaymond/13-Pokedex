0d690e9844f94ba16b563ec7bf2c309b32f84fcf
Julie <raymondjulie@proton.me>
2024-04-04 14:38:44 +0200

    created architecture + mock + pokemon class

---
+++ src/app/create-pokemon-page/create-pokemon-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-pokemon-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-pokemon-page.component.html',
+  styleUrl: './create-pokemon-page.component.scss'
+})
+export class CreatePokemonPageComponent {
+
+}
---
+++ src/app/shared/pokemons-mock.ts
@@ -0,0 +1,64 @@
+import { Pokemon } from "./pokemon";
+
+export const POKEMONS: Pokemon[] = [
+  {
+    name: "Bulbizarre",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
+    description: "Bulbizarre est un Pokémon de type plante."
+  },
+  {
+    name: "Salamèche",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+    description: "Salamèche est un Pokémon de type feu."
+  },
+  {
+    name: "Carapuce",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+    description: "Carapuce est un Pokémon de type eau."
+  },
+  {
+    name: "Aspicot",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/013.png",
+    description: "Aspicot est un Pokémon de type insecte et poison."
+  },
+  {
+    name: "Roucool",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png",
+    description: "Roucool est un Pokémon de type normal et vol."
+  },
+  {
+    name: "Rattata",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/019.png",
+    description: "Rattata est un Pokémon de type normal."
+  },
+  {
+    name: "Piafabec",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png",
+    description: "Piafabec est un Pokémon de type normal et vol."
+  },
+  {
+    name: "Abo",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/023.png",
+    description: "Abo est un Pokémon de type poison."
+  },
+  {
+    name: "Pikachu",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
+    description: "Pikachu est un Pokémon de type électrik."
+  },
+  {
+    name: "Sabelette",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/027.png",
+    description: "Sabelette est un Pokémon de type normal."
+  },
+  {
+    name: "Mélofée",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/035.png",
+    description: "Mélofée est un Pokémon de type fée."
+  },
+  {
+    name: "Groupix",
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/037.png",
+    description: "Groupix est un Pokémon de type feu."
+  }
+];
